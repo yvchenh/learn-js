7d04f9e0dc486f69d9986224bad4d300dbedc7b0
yvchenh <156022112+yvchenh@users.noreply.github.com>
2024-02-02 22:07:43 -0500

    Update manipulatedom.js

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,30 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createBtnNode(btnTxt,e,l){
+  let btnNode = document.createElement("button");
+  let btnNodeText = createTxtNode(btnText);
+  btnNode.appendChild(btnNodeText);
+  btnNode.addEventListener(e,l);
+  return btnNode;
+}
+
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(creatBtnNode("Edit text","click",function(){
+      edit(col1);
+    }));
+    tableNode.appendChild(createTRNode([col1,col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function edit(node){
+  let newNode = document.createElement("input");
+  newNode.type = 'text';
+  newNode.placeholder = 'Enter Cell (x,y)...';
+  node.replaceChild(newNode,node.childNodes[0]);
 }
